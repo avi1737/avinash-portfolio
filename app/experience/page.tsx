39a61826ee02e280fc51994b39a3da92f39eba1f
avi1737 <avinashvarpeti1@gmail.com>
2024-11-28 00:47:32 +0530

    fix build

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,40 +1,39 @@
-import ExpCard from './ExpCard';
-import { ExperienceItem } from '../types/experience';
-import StealthLogo from '../assets/stealth_design_logo.jpeg';
-import KoyalTechLogo from '../assets/koyal_logo.jpeg';
-import EmployeeForumsLogo from '../assets/employeeforums_logo.jpeg';
+// import { ExperienceItem } from '../types/experience';
+// import StealthLogo from '../assets/stealth_design_logo.jpeg';
+// import KoyalTechLogo from '../assets/koyal_logo.jpeg';
+// import EmployeeForumsLogo from '../assets/employeeforums_logo.jpeg';
 import PageHeader from '../components/ui/PageHeader';
 
 // Mock data function
-async function getExperienceData(): Promise<ExperienceItem[]> {
-  // Replace this with API or database calls in a real application.
-  return [
-    {
-      logo: StealthLogo,
-      name: 'Stealth Global',
-      companyLink: 'https://companyone.com',
-      designation: 'Software Engineer',
-      description: 'Worked on building scalable web applications.',
-      timeline: 'Jan 2020 - Dec 2022',
-    },
-    {
-      logo: KoyalTechLogo,
-      name: 'Koyal Tech',
-      companyLink: 'https://companytwo.com',
-      designation: 'Frontend Developer',
-      description: 'Focused on creating user-friendly interfaces.',
-      timeline: 'Mar 2018 - Dec 2019',
-    },
-    {
-      logo: EmployeeForumsLogo,
-      name: 'Employee Forums',
-      companyLink: 'https://companytwo.com',
-      designation: 'Frontend Developer',
-      description: 'Focused on creating user-friendly interfaces.',
-      timeline: 'Mar 2018 - Dec 2019',
-    },
-  ];
-}
+// async function getExperienceData(): Promise<ExperienceItem[]> {
+//   // Replace this with API or database calls in a real application.
+//   return [
+//     {
+//       logo: StealthLogo,
+//       name: 'Stealth Global',
+//       companyLink: 'https://companyone.com',
+//       designation: 'Software Engineer',
+//       description: 'Worked on building scalable web applications.',
+//       timeline: 'Jan 2020 - Dec 2022',
+//     },
+//     {
+//       logo: KoyalTechLogo,
+//       name: 'Koyal Tech',
+//       companyLink: 'https://companytwo.com',
+//       designation: 'Frontend Developer',
+//       description: 'Focused on creating user-friendly interfaces.',
+//       timeline: 'Mar 2018 - Dec 2019',
+//     },
+//     {
+//       logo: EmployeeForumsLogo,
+//       name: 'Employee Forums',
+//       companyLink: 'https://companytwo.com',
+//       designation: 'Frontend Developer',
+//       description: 'Focused on creating user-friendly interfaces.',
+//       timeline: 'Mar 2018 - Dec 2019',
+//     },
+//   ];
+// }
 
 const Experience = async () => {
   return (
